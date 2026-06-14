9e369c80ab9fbda2cf76f8912b35865fac1bb316
Dilkhush Kumar <dilkhushkumar@Dilkhushs-MacBook-Air.local>
2026-06-14 22:18:35 +0530

    fix: dynamic asset base path and SPA rewrite rules

--- react-blog/vite.config.js
+++ react-blog/vite.config.js
@@ -1,8 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/dilkhush-portfolio/',
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dilkhush-portfolio/',
+  // FIX: If building on GitHub Actions, use subfolder. If on Vercel or local, use root!
+  base: process.env.VERCEL || !process.env.GITHUB_ACTIONS ? '/' : '/dilkhush-portfolio/',
 })
